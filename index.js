27080d8dca19edd7251e7a0e7cbf8b7545465a74
Ragini <raginiv743@gmail.com>
2024-01-20 19:38:49 +0530

    wrap our project into array

--- index.js
+++ index.js
@@ -1,16 +1,69 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-  //do work
-  const expand = document.getElementById("expand");
-  const mobileNav = document.querySelector(".mobile-nav");
-  const ulNavMobile = document.querySelector('.nav-ul-link') ;
+
+const expand = document.getElementById("expand");
+const mobileNav = document.querySelector(".mobile-nav");
+const ulNavMobile = document.querySelector('.nav-ul-link') ;
 
 
-  expand.addEventListener("click", (event) => {
-    mobileNav.classList.toggle("hidden");
-  });
-
-  ulNavMobile.addEventListener('click', () => {
-    mobileNav.classList.toggle("hidden");
-  })
-
+expand.addEventListener("click", (event) => {
+  mobileNav.classList.toggle("hidden");
 });
+
+ulNavMobile.addEventListener('click', () => {
+  mobileNav.classList.toggle("hidden");
+})
+
+
+
+ const data = [
+  {
+    src : "./project-image.png",
+    text1: " Developed youtube app using youtube-API in react.",
+    text2:"  implemented YouTube features like the homepage recommended videos,search & explore, playlists display etc.",
+    url:"https://github.com/ragini743/youtube" ,
+  },
+    {
+      src: "./project2-image.png " ,
+      text1 :"  Developed movie web app with features listings, searching an details.",
+      text2:"",
+      url:"https://github.com/ragini743/react-project/tree/main/project-1",
+    },
+  {
+    src:"./project3-image.png",
+    text1:"  Developed youtube app using youtube-API in react.",
+    text2: "  implemented YouTube features like the homepage recommended videos,search & explore, playlists display etc.",
+  url:"https://github.com/ragini743/react/tree/master/assignment-12",
+  },
+  
+
+]
+
+
+const addProject = document.getElementById("add-project")
+console.log(addProject)
+data.forEach((project)=>{
+  console.log(project)
+  const projectDiv = document.createElement("div")
+  projectDiv.innerHTML = `<div class="pb-20 md:flex md:justify-between md:py-10 md:mx-auto border-gray border-2 mb-10"
+>
+  <div class="border-gray-100 shadow-lg border-2 md:w-[45%]">
+    <img src= ${project.src} />
+  </div>
+  <div class="pt-10 md:w-[45%]">
+    <p class="pb-4 px-2">${project.text1}
+    <br />
+   ${project.text2}
+    </p>
+    <div class="flex justify-center">
+      <button
+        type="button"
+        class="bg-indigo-500 text-white px-10 py-1 rounded-md font-bold"
+      >
+        <a href= ${project.url} class="">Link</a>
+      </button>
+    </div>
+  </div>
+</div>`
+addProject.appendChild(projectDiv)
+  
+
+})
